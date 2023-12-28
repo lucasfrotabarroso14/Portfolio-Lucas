93b1f9d69ff6c52f33e62437ade5a78a5ea10d9b
lucasfrotabarroso <lfrota@guarany.com.br>
2023-12-28 19:32:05 -0300

    deixando as tags de tecnologia dos cards dinamcias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,12 +14,17 @@ export class AppComponent {
   descricaoDoProjeto01 = "<p>O <strong>Task Manager</strong> é uma aplicação <strong>Full-Stack</strong> desenvolvida de um gerenciador de tasks. No front-end, utilizei <strong>Angular</strong> para criar uma interface dinâmica, enquanto no back-end, implementei soluções robustas utilizando <strong>Django</strong> com <strong>Python</strong></p>"
   linkGithubProjeto01 : string ="https://github.com/lucasfrotabarroso14/Task-Manager-RPG-Frontend"
   linkVercelProjeto01 : string="https://task-manager-rpg-frontend.vercel.app/"
+  tagsProjeto01 : Array<string> = ["Back-end","Front-End","Python","JavaScript"]
+  // tags : Array<string> = ["teste","teste","teste","teste"]
 
   //------------PROJETO 02 Finova------------------------------------------------------
   imgProjeto02 : string = "../assets/finova-branco-logo.png" 
   nomeProjeto02 ="Finova Back-End"
   descricaoDoProjeto02 = "<p>O <strong>Task Manager</strong> é uma aplicação full-stack desenvolvida de um gerenciador de tasks. No front-end, utilizei <strong>Angular</strong> para criar uma interface dinâmica, enquanto no back-end, implementei soluções robustas utilizando <strong>Django</strong> com <strong>Python</strong></p>"
   linkGithubProjeto02 : string ="https://github.com/lucasfrotabarroso14/Finova-Portal-Backend"
+
+  tagsProjeto02 : Array<string> = ["Back-end","Python","Flask","API"]
+
 
 
 
--- src/app/card-project/card-project.component.ts
+++ src/app/card-project/card-project.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-card-project',
   templateUrl: './card-project.component.html',
   styleUrls: ['./card-project.component.scss']
 })
-export class CardProjectComponent {
+export class CardProjectComponent implements OnChanges{
 
   @Input() img !: string 
   @Input() nomeProjeto!:string
@@ -13,9 +13,29 @@ export class CardProjectComponent {
   dialog !: boolean 
   @Input() linkGithubFilho !: string
   @Input() linkVercelFilho !: string
+  @Input() tagsFilho !: Array<string>
+  
+  tagsPares:Array<string> = [];
+  tagsImpares : Array <string> = [];
 
-  constructor(){
+  tagsIntercaladas: Array<{ tag: string, par: boolean }> = [];
 
+
+  constructor() {
+    
+
+  }
+  ngOnChanges() {
+    this.dividirTags()
+    this.intercalarTags();
+      
+  }
+
+  dividirTags(){
+    if (this.tagsFilho) {
+      this.tagsPares = this.tagsFilho.filter((_, index) => index % 2 === 0);
+      this.tagsImpares = this.tagsFilho.filter((_, index) => index % 2 !== 0);
+    }
   }
 
   openOrCloseDialog(){
@@ -24,8 +44,18 @@ export class CardProjectComponent {
     
   }
 
+  intercalarTags() {
+    this.tagsIntercaladas = [];
+    const maiorTamanho = Math.max(this.tagsPares.length, this.tagsImpares.length);
 
-  
-  
-
+    for (let i = 0; i < maiorTamanho; i++) {
+      if (i < this.tagsPares.length) {
+        this.tagsIntercaladas.push({ tag: this.tagsPares[i], par: true });
+      }
+      if (i < this.tagsImpares.length) {
+        this.tagsIntercaladas.push({ tag: this.tagsImpares[i], par: false });
+      }
+    }
+  }
 }
+
