2f57e809b773ff897cbd64d68ba0ee3099ae29c6
lucasfrotabarroso <lfrota@guarany.com.br>
2023-12-28 21:05:37 -0300

    alter finova description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   //------------PROJETO 01 Task Manager------------------------------------------------------
   imgProjeto01 : string = "../assets/task-manager.png" 
   nomeProjeto01 ="Task Manager"
-  descricaoDoProjeto01 = "<p>O <strong>Task Manager</strong> é uma aplicação <strong>Full-Stack</strong> desenvolvida de um gerenciador de tasks. No front-end, utilizei <strong>Angular</strong> para criar uma interface dinâmica, enquanto no back-end, implementei soluções robustas utilizando <strong>Django</strong> com <strong>Python</strong></p>"
+  descricaoDoProjeto01 = "<p>O <strong>Task Manager</strong> é uma aplicação <strong>Full-Stack</strong> desenvolvida de um gerenciador de tasks. No Front-End, utilizei <strong>Angular</strong> para criar uma interface dinâmica, enquanto no Back-End, implementei soluções robustas utilizando <strong>Django</strong> com <strong>Python</strong></p>"
   linkGithubProjeto01 : string ="https://github.com/lucasfrotabarroso14/Task-Manager-RPG-Frontend"
   linkVercelProjeto01 : string="https://task-manager-rpg-frontend.vercel.app/"
   tagsProjeto01 : Array<string> = ["Back-end","Front-End","Python","JavaScript"]
@@ -20,7 +20,8 @@ export class AppComponent {
   //------------PROJETO 02 Finova------------------------------------------------------
   imgProjeto02 : string = "../assets/finova-branco-logo.png" 
   nomeProjeto02 ="Finova Back-End"
-  descricaoDoProjeto02 = "<p>O <strong>Task Manager</strong> é uma aplicação full-stack desenvolvida de um gerenciador de tasks. No front-end, utilizei <strong>Angular</strong> para criar uma interface dinâmica, enquanto no back-end, implementei soluções robustas utilizando <strong>Django</strong> com <strong>Python</strong></p>"
+ 
+  descricaoDoProjeto02 = "<p>O <strong>Finova</strong> é um sistema <strong>Back-End</strong> desenvolvido para gerenciamento de ativos e portfólios de investimentos. Construído com <strong>Python</strong> e o framework <strong>Flask</strong>, oferece uma <strong> API RESTful</strong> segura e eficiente. Inclui recursos como autenticação <strong>JWT</strong>, integração com <strong> MySQL</strong>, e serviços de email para confirmação de registro.</p>"
   linkGithubProjeto02 : string ="https://github.com/lucasfrotabarroso14/Finova-Portal-Backend"
 
   tagsProjeto02 : Array<string> = ["Back-end","Python","Flask","API"]
